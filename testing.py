fa8d7c6b3d8bc0404af23a42b992831b8799c9c4
Leg3ndary <ben.zhou.2006@gmail.com>
2024-05-16 13:33:16 -0400

    I'm stupid

--- testing.py
+++ testing.py
@@ -3,10 +3,5 @@ import os
 import dotenv
 dotenv.load_dotenv()
 
-client = redis.Redis(
-  host='redis-16508.c253.us-central1-1.gce.cloud.redislabs.com',
-  port=16508,
-  password='z6vIXisQfDGwTf4sisq7fHj7bvDQAkfd',
-  decode_responses=True)
 
 print(client.get("uses"))
